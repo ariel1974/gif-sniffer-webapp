5b934c11f0e2434d2f8254ded1316ccb653e028e
arielillouz <ariel.illouz@gmail.com>
2017-12-06 17:08:04 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var http = require('http');
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var fs = require('fs');
 
-app.use('/images', express.static('images'))
-app.use('/css', express.static('css'))
-app.use('/favicons', express.static('favicons'))
+app.use('/images', express.static('images'));
+app.use('/css', express.static('css'));
+app.use('/favicons', express.static('favicons'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
