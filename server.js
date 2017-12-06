7f300b924256988f2d7fa096be313ef9d98575b7
arielillouz <ariel.illouz@gmail.com>
2017-12-06 17:22:44 +0200

    initial commit

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var fetch = require('node-fetch');
 
 app.use('/images', express.static('images'));
 app.use('/css', express.static('css'));
 app.use('/favicons', express.static('favicons'));
+
+app.get('/top_risky_repos', (req, res) => {
+
+    fetch('http://localhost:8123/top_risky_repos')
+        .then(function (data) {
+            return data.json();
+        }).then(function (json) {
+
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(json, null, 3) );
+
+    });
+});
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
