85ab1cfe3872069615b95c8c4c750ccb4d81c3bb
arielillouz <ariel.illouz@gmail.com>
2017-12-06 17:05:49 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var express = require('express')
+var app = express()
+var fs = require('fs');
+
+app.use('/images', express.static('images'))
+app.use('/css', express.static('css'))
+app.use('/favicons', express.static('favicons'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
